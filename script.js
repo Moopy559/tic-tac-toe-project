ded494d6dea2cdd5b7f34f6ce6f8d28a16536d1a
Moopy559 <125260487+Moopy559@users.noreply.github.com>
2024-01-16 13:38:11 +1000

    Refactor game logic and add logic to prevent players from using spots that area already taken

--- script.js
+++ script.js
@@ -19,56 +19,55 @@ function createPlayer(name, marker, value) {
 let playerOne = createPlayer("Player One", "X", 1);
 let playerTwo = createPlayer("Player Two", "O", 2);
 
-let gameFlow = {
-  isP1Turn: true,
+let gameflow = {
+  turn: playerOne,
   isP1Win: false,
   isP2Win: false,
   p1Marker: playerOne.marker,
   p2Marker: playerTwo.marker,
   getTurn: function () {
-    if (this.isP1Turn === true) {
+    if (this.turn === playerOne) {
       console.log("It is " + playerOne.name + "'s turn");
     } else {
       console.log("It is " + playerTwo.name + "'s turn");
     }
   },
   updateGrid: function (row, column, value) {
-    gameboard.grid[row][column] = value;
+    if (gameboard.grid[row][column] === 0) {
+      gameboard.grid[row][column] = value;
+    } else {
+      console.log("Sorry, that spot is taken, please try again");
+      this.playerTurn();
+    }
   },
   nextTurn: function () {
-    if (this.isP1Turn === true) {
+    this.getTurn();
+    if (this.turn === playerOne) {
       // Player One's Turn
-      this.updateGrid(
-        prompt("Player One's turn - Choose a row (0, 1 or 2)"),
-        prompt("Player One's turn - Choose a column (0, 1 or 2)"),
-        playerOne.value
-      );
-      gameFlow.isP1Turn = false;
-      console.log(gameboard.grid);
-      this.nextTurn();
+      this.p1Turn();
     } else {
       this.updateGrid(
         prompt("Player Two's turn - Choose a row (0, 1 or 2)"),
         prompt("Player Two's turn - Choose a column (0, 1 or 2)"),
         playerTwo.value
       );
-      gameFlow.isP1Turn = true;
+      gameflow.turn = playerOne;
       console.log(gameboard.grid);
       this.nextTurn();
     }
   },
-  p1Turn: function () {
+  playerTurn: function () {
     this.updateGrid(
-      prompt("which row?"),
-      prompt("which column?"),
-      playerOne.value
+      prompt(gameflow.turn.name + "'s turn - Choose a row (0, 1 or 2)"),
+      prompt(gameflow.turn.name + "'s turn - Choose a column (0, 1 or 2)"),
+      gameflow.turn.value
     );
-  },
-  p2Turn: function () {
-    this.updateGrid(
-      prompt("which row?"),
-      prompt("which column?"),
-      playerTwo.value
-    );
+    if (gameflow.turn === playerOne) {
+      gameflow.turn = playerTwo;
+    } else {
+      gameflow.turn = playerOne;
+    }
+    console.log(gameboard.grid);
+    this.playerTurn();
   },
 };
