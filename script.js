20b6e75bf7ec896745d871a5b7b2180eaef4912b
Moopy559 <125260487+Moopy559@users.noreply.github.com>
2024-01-16 12:04:06 +1000

    Add 'next turn' logic that also updates the gameboard grid accordingly

--- script.js
+++ script.js
@@ -8,15 +8,16 @@ let gameboard = {
   ],
 };
 
-function createPlayer(name, marker) {
+function createPlayer(name, marker, value) {
   return {
     name,
     marker,
+    value,
   };
 }
 
-let playerOne = createPlayer("Player One", "X");
-let playerTwo = createPlayer("Player Two", "O");
+let playerOne = createPlayer("Player One", "X", 1);
+let playerTwo = createPlayer("Player Two", "O", 2);
 
 let gameFlow = {
   isP1Turn: true,
@@ -24,4 +25,50 @@ let gameFlow = {
   isP2Win: false,
   p1Marker: playerOne.marker,
   p2Marker: playerTwo.marker,
+  getTurn: function () {
+    if (this.isP1Turn === true) {
+      console.log("It is " + playerOne.name + "'s turn");
+    } else {
+      console.log("It is " + playerTwo.name + "'s turn");
+    }
+  },
+  updateGrid: function (row, column, value) {
+    gameboard.grid[row][column] = value;
+  },
+  nextTurn: function () {
+    if (this.isP1Turn === true) {
+      // Player One's Turn
+      this.updateGrid(
+        prompt("Player One's turn - Choose a row (0, 1 or 2)"),
+        prompt("Player One's turn - Choose a column (0, 1 or 2)"),
+        playerOne.value
+      );
+      gameFlow.isP1Turn = false;
+      console.log(gameboard.grid);
+      this.nextTurn();
+    } else {
+      this.updateGrid(
+        prompt("Player Two's turn - Choose a row (0, 1 or 2)"),
+        prompt("Player Two's turn - Choose a column (0, 1 or 2)"),
+        playerTwo.value
+      );
+      gameFlow.isP1Turn = true;
+      console.log(gameboard.grid);
+      this.nextTurn();
+    }
+  },
+  p1Turn: function () {
+    this.updateGrid(
+      prompt("which row?"),
+      prompt("which column?"),
+      playerOne.value
+    );
+  },
+  p2Turn: function () {
+    this.updateGrid(
+      prompt("which row?"),
+      prompt("which column?"),
+      playerTwo.value
+    );
+  },
 };
